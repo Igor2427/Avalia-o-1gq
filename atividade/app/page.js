24381281b2865675e2ebdd56b4feac85070f97fc
Igor2427 <128043201+Igor2427@users.noreply.github.com>
2025-09-25 01:28:17 +0000

    fundo

--- atividade/app/page.js
+++ atividade/app/page.js
@@ -2,8 +2,17 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div style={{ display: "flex", padding: "20px", gap: "20px", alignItems: "flex-start" }}>
-      
+    <div
+      style={{
+        display: "flex",
+        padding: "20px",
+        gap: "20px",
+        alignItems: "flex-start",
+        minHeight: "100vh",
+        background: "linear-gradient(to bottom, #0a192f, #112240, #0a192f)", 
+        color: "var(--color-text)", 
+      }}
+    >
       <div style={{ display: "flex", flexDirection: "column", alignItems: "center", gap: "20px" }}>
         <img
           src="/assets/images/Foto.jpg"
@@ -11,52 +20,58 @@ export default function Home() {
           alt="Foto de perfil"
         />
         <Link
-  href="/jogo"
-  style={{
-    padding: "10px 20px",
-    backgroundColor: "#64ffda",
-    color: "#0A192F",
-    textDecoration: "none",
-    borderRadius: "8px",
-    fontWeight: "bold",
-    display: "inline-block",
-    textAlign: "center"
-  }}
->
-  Jogar Forca
-</Link>
-
+          href="/jogo"
+          style={{
+            padding: "10px 20px",
+            backgroundColor: "var(--color-accent)",
+            color: "#0A192F",
+            textDecoration: "none",
+            borderRadius: "8px",
+            fontWeight: "bold",
+            display: "inline-block",
+            textAlign: "center"
+          }}
+        >
+          Jogar Forca
+        </Link>
       </div>
 
-      <table border="1" style={{ borderCollapse: "collapse", width: "500px" }}>
-        <caption style={{ captionSide: "top", fontWeight: "bold", padding: "8px" }}>
-          Currículo
-        </caption>
-        <tbody>
-          <tr>
-            <th style={{ padding: "8px", textAlign: "left" }}>Nome: Igor Gabriel Lira Uchôa</th>
-          </tr>
-          <tr>
-            <th style={{ padding: "8px", textAlign: "left" }}>Idade: 20</th>
-          </tr>
-          <tr>
-            <th style={{ padding: "8px", textAlign: "left" }}>Sobre:</th>
-          </tr>
-          <tr>
-            <td style={{ padding: "8px", textAlign: "justify" }}>
-              Nasci no estado de Pernambuco na cidade de Recife, me formei no colégio Damas da instrução Cristã no ano de 2022 e atualmente estou cursando o curso de Ciência da Computação na Unicap (5º período)
-            </td>
-          </tr>
-          <tr>
-            <th style={{ padding: "8px", textAlign: "left" }}>Tecnologias:</th>
-          </tr>
-          <tr>
-            <td style={{ padding: "8px", textAlign: "justify" }}>
-              Python, Java, C, CSS.
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <table 
+  style={{
+    borderCollapse: "collapse",
+    width: "500px",
+    border: "2px solid black" 
+  }}
+>
+  <caption style={{ captionSide: "top", fontWeight: "bold", padding: "8px" }}>
+    Currículo
+  </caption>
+  <tbody>
+    <tr>
+      <th style={{ padding: "8px", textAlign: "left", borderBottom: "2px solid black" }}>Nome: Igor Gabriel Lira Uchôa</th>
+    </tr>
+    <tr>
+      <th style={{ padding: "8px", textAlign: "left", borderBottom: "2px solid black" }}>Idade: 20</th>
+    </tr>
+    <tr>
+      <th style={{ padding: "8px", textAlign: "left", borderBottom: "2px solid black" }}>Sobre:</th>
+    </tr>
+    <tr>
+      <td style={{ padding: "8px", textAlign: "justify", borderBottom: "2px solid black" }}>
+        Nasci no estado de Pernambuco na cidade de Recife, me formei no colégio Damas da instrução Cristã no ano de 2022 e atualmente estou cursando o curso de Ciência da Computação na Unicap (5º período)
+      </td>
+    </tr>
+    <tr>
+      <th style={{ padding: "8px", textAlign: "left", borderBottom: "2px solid black" }}>Tecnologias:</th>
+    </tr>
+    <tr>
+      <td style={{ padding: "8px", textAlign: "justify", borderBottom: "2px solid black" }}>
+        Python, Java, C, CSS.
+      </td>
+    </tr>
+  </tbody>
+</table>
+
     </div>
   );
 }
